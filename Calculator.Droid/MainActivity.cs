a313bc927c0fbf12443b39a8df527eef8bc1702d
Apple <apple@Apples-MacBook-Pro.local>
2018-08-11 23:09:03 +0530

    first commit

---
+++ Calculator.Droid/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Calculator.Droid
+{
+    [Activity(Label = "Calculator", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AppCenter.Start("0b645681-07a0-4e3b-86ca-585c8a73164f", typeof(Analytics), typeof(Crashes));
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
